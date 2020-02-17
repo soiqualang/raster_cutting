d01f3b78b0c28fd080f80824bafc9c0987e5e914
soiqualang <soiqualang.chentreu@gmail.com>
2020-02-17 09:59:52 +0700

    fix raster_cutting_tiff_folder.py

--- raster_cutting_tiff_folder.py
+++ raster_cutting_tiff_folder.py
@@ -8,11 +8,11 @@ from pathlib import Path
 # Em config cho lai lai nhe!
 
 # Thu muc chua anh can cat
-foin = Path('C:/Users/soiqu/Desktop/2000/')
+foin = Path('C:/Users/soiqu/Desktop/test_pyQGIS/2000/')
 # Duong dan den shapefile lam ranh cat
-shp_path='C:/Users/soiqu/Desktop/shp/gialai.shp'
+shp_path='C:/Users/soiqu/Desktop/test_pyQGIS/shp/gialai.shp'
 # Thu muc luu ket qua
-foout='C:/Users/soiqu/Desktop/cutting/'
+foout='C:/Users/soiqu/Desktop/test_pyQGIS/kq/'
 
 
 def getFname(path):
@@ -26,6 +26,6 @@ def getFname(path):
 for raster_file in foin.glob('*.tif'):
     # print(raster_file)
     #cmd='"gdalwarp -of GTiff -dstnodata -9999.0 -overwrite -cutline '+str(shp_path)+' -crop_to_cutline '+str(raster_file)+' '+getFname(str(raster_file))+'"'
-    cmd='"gdalwarp" -cutline "'+str(shp_path)+'" -crop_to_cutline -of GTiff -dstnodata -9999.0 -overwrite "'+str(raster_file)+'" "'+fout+'t_'+getFname(str(raster_file))+'.tif"'
+    cmd='"gdalwarp" -cutline "'+str(shp_path)+'" -crop_to_cutline -of GTiff -dstnodata -9999.0 -overwrite "'+str(raster_file)+'" "'+foout+'t_'+getFname(str(raster_file))+'.tif"'
     #print(cmd)
     subprocess.Popen(cmd,shell=True)
